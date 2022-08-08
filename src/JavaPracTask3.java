fc4edc270a2ba977a6bdb79255ccdf11f5a68838
ArchanaKarnam <karnamarchana@gmail.com>
2022-08-08 16:45:41 +0530

    task3

---
+++ src/JavaPracTask3.java
@@ -0,0 +1,14 @@
+
+public class JavaPracTask3 {
+    public static void main(String[] args){
+        for(int i=0;i<=3;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("1");
+            }
+            for(int j=i+1;j<=3;j++){
+                System.out.print("2");
+            }
+            System.out.println();
+        }
+    }
+}
