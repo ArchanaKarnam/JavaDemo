16890ad0eb26e15c2f3865538bf7bcb2d445e9f8
ArchanaKarnam <karnamarchana@gmail.com>
2022-08-08 15:28:26 +0530

    task1

--- JavaDemoPrac.java
+++ JavaDemoPrac.java
@@ -7,6 +7,27 @@ public class JavaDemoPrac {
         int dif1=(x-n);
         int dif2=(y-n);
         int dif3=(z-n);
+        if(dif1<0 && dif2>=0 && dif3>=0 ){
+            if(dif2<dif3){
+                System.out.println("L2");
+            }else{
+                System.out.println("L3");
+            }
+        }
+        if(dif1>=0 && dif2<0 && dif3>=0 ){
+            if(dif1<dif3){
+                System.out.println("L1");
+            }else{
+                System.out.println("L3");
+            }
+        }
+        if(dif1>=0 && dif2>=0 && dif3<0 ){
+            if(dif1<dif2){
+                System.out.println("L1");
+            }else{
+                System.out.println("L2");
+            }
+        }
         if(dif1<=dif2 && dif1<=dif3 && dif1>=0 && dif2>=0 && dif3>=0){
             System.out.println("L1");
         }
